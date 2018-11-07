bc165581b868c6be8b471b3e0b63c9330fdb9434
ThomasBisson <bisson.th@gmail.com>
2018-11-07 14:19:36 +0100

    Tab finished

---
+++ Assets/scripts/UI/Tab.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tab : MonoBehaviour {
+
+    [HideInInspector]
+    public GameObject m_content;
+    [HideInInspector]
+    public Button m_button;
+    [HideInInspector]
+    public Image m_image;
+
+    // Use this for initialization
+    void Start () {
+        m_image = GetComponent<Image>();
+        m_button = GetComponent<Button>();
+        m_content = transform.GetChild(1).gameObject;
+	}
+	
+}
---
+++ Assets/scripts/UI/TabController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabController : MonoBehaviour {
+
+    public Tab[] m_tabs;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Tab tab in m_tabs)
+        {
+            tab.m_button.onClick.AddListener(delegate
+            {
+                HideAllPanel();
+                DisplayPanel(tab);
+            });
+        }
+        HideAllPanel();
+        DisplayPanel(m_tabs[0]);
+	}
+
+    private void DisplayPanel(Tab tab)
+    {
+        tab.m_content.SetActive(true);
+        var tempColor = tab.m_image.color;
+        tempColor.a = 1f;
+        tab.m_image.color = tempColor;
+    }
+
+
+    private void HideAllPanel()
+    {
+        foreach (Tab tab in m_tabs)
+        {
+            tab.m_content.SetActive(false);
+            var tempColor = tab.m_image.color;
+            tempColor.a = .5f;
+            tab.m_image.color = tempColor;
+        }
+    }
+
+
+	
+}
